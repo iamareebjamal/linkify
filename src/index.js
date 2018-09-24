fe3a5d3a69dcdd17024960baa55d559a995121ba
Areeb Jamal <jamal.areeb@gmail.com>
2018-09-24 11:04:50 +0530

    Add index.html for testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import urlRegex from './regex.js'
 
-console.log(urlRegex);
+const selector = '.linkify'
+
+const linkify = url => `<a class='postUrl' href='${url}'' target='_blank'>${url}</a>`
+
+function convertElements(elements) {
+    elements.each(function() {
+        this.innerHTML = this.textContent.replace(urlRegex, linkify)
+    });
+}
+
+function convert(element) {
+    let elements = null;
+    if (element == undefined) {
+        elements = $(selector)
+    } else {
+        elements = element.find(selector).addBack(selector)
+    }
+
+    convertElements(elements)
+}
+
+$.fn.linkify = function(bare) {
+    if (bare) {
+        convertElements(this)
+    } else {
+        convert(this)
+    }
+
+    return this;
+}
+
+export default {
+    linkify,
+    convert
+}
